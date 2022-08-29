3bc9f892d59a5b08bf32db8c10dbde5bd46b5b4d
MD ASHIK MAHMUD <ashik.swfu@outlook.com>
2022-08-30 00:50:00 +0800

    user

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import Chart from '../../Components/Chart/Chart';
+import FeaturedInfo from '../../Components/FeaturedInfo/FeaturedInfo';
+import WidgetLg from '../../Components/WidgetLg/WidgetLg';
+import WidgetSm from '../../Components/WidgetSm/WidgetSm';
+import { userData } from '../../dummyData';
 import "./Home.css";
 
 const Home = () => {
     return (
-        <div>
-            
+        <div className='home'>
+            <FeaturedInfo/>
+            <Chart data={userData} title="User Analytics" grid dataKey="Active User"/>
+            <div className="homeWidgets">
+                <WidgetSm/>
+                <WidgetLg/>
+            </div>
         </div>
     );
 };
